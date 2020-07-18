c1a9fbaa716625128720c4264ef64d8d782b8e75
Astemir <shibzuhov2000@mail.ru>
2020-07-18 22:36:28 +0300

    Initial Commit

---
+++ ImageDownloader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageDownloader
+//
+//  Created by Astemir Shibzuhov on 18.07.2020.
+//  Copyright © 2020 Astemir Shibzuhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
